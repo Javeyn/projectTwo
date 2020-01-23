feac4a49e11746e81d7d58dbf1463a1b5667c6fb
Austin Finke <austinfinke28@gmail.com>
2020-01-23 13:49:56 -0800

    more routes made

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -1,18 +1,18 @@
 console.log('frontend routes linked');
-$(function() {
+$(function () {
 
   // redirect to 'create account' page  
-  $('#createaccount').click( () => {
+  $('#createaccount').click(() => {
     $(location).attr('href', './create');
   })
 
   // get user info and add account to db
-  $('#create').click( () => {
+  $('#create').click(() => {
     let name = $('.name').val();
     let password = $('.password').val();
     let confirm = $('.confirm').val();
 
-    if(password !== confirm) {
+    if (password !== confirm) {
       alert('passwords must match');
     } else {
 
@@ -24,20 +24,33 @@ $(function() {
       $.ajax('/api/newaccount', {
         type: 'POST',
         data: newAccount
-      }).then( () => {
-        console.log('here');
+      }).then(() => {
+        console.log('new account added');
       })
     }
   })
 
-  $('#exit').click( () => {
-    $(location).attr('href', './');
+  $('#exit').click(() => {
+    $(location).attr('href', '/');
   })
 
-  $('#login').click( () => {
-    $.ajax('/api/account', {
-      // hmm
+  $('#login').click(() => {
+    let name = $('.username').val().trim();
+    let query = `/api/account/${name}`;
+    $.ajax(query, {
+      type: 'GET',
+    }).then(() => {
+      $(location).attr('href', './usersetup')
     })
   })
+
+
+
+
+
+
+
+
+
 
 })
