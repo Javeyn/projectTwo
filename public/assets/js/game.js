0f6b2fb4429f9c005937f66e6609d41f358a61c7
Jack Sorensen <jack.sorensen11@gmail.com>
2020-01-23 10:21:25 -0800

    fixed image paths

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -67,7 +67,7 @@ function createPlayer($container) {
   GAME_STATE.playerX = GAME_WIDTH / 2;
   GAME_STATE.playerY = GAME_HEIGHT - 50;
   const $player = document.createElement("img");
-  $player.src = "img/player-blue-1.png";
+  $player.src = "public/assets/img/player-blue-1.png";
   $player.className = "player";
   $container.appendChild($player);
   setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
@@ -151,7 +151,7 @@ function destroyLaser($container, laser) {
 
 function createEnemy($container, x, y) {
   const $element = document.createElement("img");
-  $element.src = "img/enemy-blue-1.png";
+  $element.src = "public/assets/img/enemy-blue-1.png";
   $element.className = "enemy";
   $container.appendChild($element);
   const enemy = {
@@ -192,7 +192,7 @@ function destroyEnemy($container, enemy) {
 
 function createEnemyLaser($container, x, y) {
   const $element = document.createElement("img");
-  $element.src = "img/laser-red-5.png";
+  $element.src = "public/assets/img/laser-red-5.png";
   $element.className = "enemy-laser";
   $container.appendChild($element);
   const laser = { x, y, $element };
