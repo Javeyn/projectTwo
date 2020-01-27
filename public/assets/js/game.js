e624b3c4fda4857cf9954f064455f10b47b04e29
Jack Sorensen <55419936+Javeyn@users.noreply.github.com>
2020-01-27 12:10:56 -0800

    quick fix
    asdf

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -478,7 +478,7 @@ function viewLogin() {
 }
 
 function viewHighscore() {
-  $(location).attr('href', '.highscore');
+  $(location).attr('href', './highscore');
 }
 
 window.addEventListener("keydown", onKeyDown);
