369494c9a733ab234a15b789f79c3bb6e6f19080
Jenny <j.thurnau@hotmail.com>
2020-01-27 10:55:55 -0800

    fixed buttons and routes

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -461,8 +461,8 @@ function modeSelection() {
   })
 }
 
-function viewUserSetup() {
-  $(location).attr('href', './usersetup');
+function viewMainPage() {
+  $(location).attr('href', './');
 }
 
 function viewHighscore() {
