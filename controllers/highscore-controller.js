48270887e0133e0147f7f3381cb78260e95b80ef
Nick <57273851+nick555555@users.noreply.github.com>
2020-01-27 11:08:54 -0800

    associations work with hard coded user id of 1

--- controllers/highscore-controller.js
+++ controllers/highscore-controller.js
@@ -4,12 +4,32 @@ const router = express.Router();
 
 // get all scores
 router.get('/api/newscore', (req, res) => {
-    db.Highscore.findAll().then((data) => {
+    var query = {};
+    if (req.query.account_id){
+        query.AccountId = req.query.account_id;
+    }
+    db.Highscore.findAll({
+        where: query,
+        include: [db.Account]
+    }).then((data) => {
       return res.json(data);
     })
 })
 
+router.get("/api/newscore/:id", function(req, res) {
+    db.Highscore.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Account]
+    }).then(function(data) {
+      res.json(data);
+    });
+});
+
 router.post('/api/newscore', (req, res) => {
+    // req.body.AccountId = req.session.user.id;
+    req.body.AccountId = 1;
     db.Highscore.create(req.body).then((data) => {
         return res.json(data);
     });
